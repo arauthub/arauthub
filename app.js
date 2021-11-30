ad5f6c739d75be5a52f061be4765146ce22f75b3
Abhijeet Raut <arammuapps@gmail.com>
2021-11-30 12:38:26 +0530

    assignment

---
+++ app.js
@@ -0,0 +1,28 @@
+// Instantiate iTunes Class
+const itunes = new iTunes();
+// Instantiate UI Class
+const ui = new UI();
+
+const searchBox = document.querySelector(".text-box");
+const results = document.querySelector(".results");
+
+searchBox.addEventListener("keyup", (e) => {
+  // Get Input Text
+  const userText = e.target.value;
+
+  // Check to see if the input is valid and not empty
+  if (userText !== "") {
+    // URL Encode userText
+    const encodedTextURL = encodeURIComponent(userText);
+    // Get Artist Album Data From iTunes Database
+    itunes.getArtistData(encodedTextURL).then((artistData) => {
+      // Paint the UI with artist album info
+      results.style.overflowY = "scroll";
+      ui.paintUI(artistData);
+    });
+  } else {
+    // If there is nothing in the search bar, clear the ui
+    ui.clearUI();
+    results.style.overflow = "hidden";
+  }
+});
